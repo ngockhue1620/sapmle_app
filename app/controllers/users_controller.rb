e8a2082169dea9bb795b9fb24d7361872ed4ab55
ngockhue1620 <ngockhuentca2k@gmail.com>
2021-11-10 17:03:36 +0700

    chapter_7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,32 @@
 class UsersController < ApplicationController
-  def new; end
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user.present?
+    flash[:warning] = t "not_found"
+    redirect_to root_url
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
+
 end
